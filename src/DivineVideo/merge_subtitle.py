b5b7ab6ae9dd0a388587454fba771468c4438b48
kevinleung <ocean_talk@outlook.com>
2025-02-04 00:05:08 +0800

    feat: 视频翻译实现

---
+++ src/DivineVideo/merge_subtitle.py
@@ -0,0 +1,7 @@
+import subprocess
+
+def merge_subtitle(srt_file, input_video, output_video):
+    cmd = ["ffmpeg", "-i", input_video, "-vf", f"subtitles={srt_file}:force_style='FontName=Arial,FontSize=14,PrimaryColour=&HFFFFFF,Outline=1,OutlineColour=&H000000'", "-c:a", "copy", output_video, "-y"]
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False, text=True)
+    for line in process.stdout:
+        print(line)
